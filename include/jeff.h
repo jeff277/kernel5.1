a44a5f1ad3fc24632568ea8f6a39ed3ea92dd654
flead <lifei@MacOS-jeff.local>
2021-09-08 18:28:10 +0800

    tcp三次握手

---
+++ include/jeff.h
@@ -0,0 +1,15 @@
+/*
+ * Kongwen
+ */
+
+
+#ifndef KERNEL_JEFF_H
+#define KERNEL_JEFF_H
+
+#define __KERNEL__ 1
+
+#define CONFIG_INET 1
+
+#define CONFIG_ARCH_HAS_REFCOUNT 1
+
+#endif //KERNEL_JEFF_H
